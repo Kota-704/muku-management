729293328fc57f1948cd9b001fc12ba0ef09a533
Kota-704 <kktt14590358@gmail.com>
2025-02-17 23:45:17 +0900

    ♻️ /微修正

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import CalendarBody from "@/components/calendarBody";
 export default function Home() {
   return (
     <div className="wrapper p-3 min-h-screen">
-      <h1 className="text-center mt-4">椋ちゃんの管理アプリ</h1>
+      <h1 className="text-center mt-4">椋の管理アプリ</h1>
       <button className="absolute top-6 right-3">
         <Image
           src="/settings-2-svgrepo-com.svg"
